30dbf35e7c9cf2a261f846c24c9d91e6dd55bae3
Char Aznable <aznable.char.0083@gmail.com>
2021-10-09 00:02:23 -0700

    Enhancement: refactor tests
    This applies a fixture parametrized by the view layout types to each
    test of view property so that the original 3 test modules are merged
    into one.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,48 @@
 # Description: Utilities for pretty-printing Kokkos::View
 #
 # Distributed under terms of the 3-clause BSD license.
+import numpy as np
 import re
 import subprocess
 import pytest
 import textwrap
 
+@pytest.fixture(scope="module", params=["Kokkos::LayoutRight",
+                                        "Kokkos::LayoutLeft",
+                                        "Kokkos::LayoutStride"])
+def foreachViewLayout(request):
+    cpp = getattr(request.module, "cpp", "")
+    layout = request.param
+    shape = getattr(request.module, "shape", (3, 4, 5))
+    # For testing purpose, we fix the strides in the case of
+    # Kokkos::LayoutStride. For LayoutLeft and LayoutRight, we set the
+    # strides to be the expected ones
+    strides = (1, 3, 15)
+    if layout == "Kokkos::LayoutRight":
+        strides = np.ones_like(shape, dtype=int)
+        strides[1:] = shape[::-1][:-1]
+        strides = tuple(np.cumprod(strides)[::-1])
+    elif layout == "Kokkos::LayoutLeft":
+        strides = np.ones_like(shape, dtype=int)
+        strides[1:] = shape[:-1]
+        strides = tuple(np.cumprod(strides))
+    ctorStride = ",".join(
+        map(str, [ i for t in zip(shape, strides) for i in t ]))
+    ctorShape = ",".join(map(str, shape))
+    # Set the C++ template parameter for layout
+    cpp = re.sub(r"/\*TestViewLayoutTparam\*/", layout, cpp)
+    # Set the ctor for the view defining the shape and strides
+    ctor = {"Kokkos::LayoutRight" : ctorShape,
+        "Kokkos::LayoutLeft" : ctorShape,
+        "Kokkos::LayoutStride" : f"{layout}({ctorStride})",
+        }
+    cpp = re.sub(r"/\*TestViewLayoutCtor\*/", ctor[layout], cpp)
+
+    yield layout, shape, strides, cpp
+
+
 @pytest.fixture(scope="module")
-def writeCPP(tmpdir_factory, request):
+def writeCPP(tmpdir_factory, foreachViewLayout):
     """Write a c++ source file for gdb to examine the Kokkos::View
 
     Args:
@@ -22,20 +57,18 @@ def writeCPP(tmpdir_factory, request):
     Returns: TODO
 
     """
-    shape = getattr(request.module, "shape", (3,4,5))
-    layout = getattr(request.module, "layout", "Kokkos::LayoutRight")
-    cpp = getattr(request.module, "cpp", "")
+    layout, shape, strides, cpp = foreachViewLayout
     fnShape = "-".join(map(str, shape))
     fnLayout = re.sub(r"::", "", layout)
     p = tmpdir_factory.mktemp(f"TestView{fnLayout}{fnShape}")
     fCXX = p.join("test.cpp")
     fCXX.write(textwrap.dedent(cpp))
-    yield (p, fCXX)
+    yield (p, fCXX, layout, shape, strides)
 
 
 @pytest.fixture(scope="module")
 def generateCMakeProject(writeCPP):
-    p, _ = writeCPP
+    p, _, layout, shape, strides = writeCPP
     fCMakeLists = p.join("CMakeLists.txt")
     fCMakeLists.write(textwrap.dedent(
         """
@@ -80,12 +113,15 @@ def generateCMakeProject(writeCPP):
                                encoding='utf-8')
         assert rMake.returncode == 0, f"Error with running make: {rMake.stderr}"
     fTest = fBuildDir.join("test")
-    yield (p, fBuildDir, fTest)
+    yield (p, fBuildDir, fTest, layout, shape, strides)
 
 
-@pytest.fixture
-def runGDB():
-    def _runGDB(content : str, fPath, executable : str):
+@pytest.fixture(scope="function")
+def runGDB(request):
+    content = getattr(request.module, "gdbinit", "")
+    def _runGDB(gdbComms : str, fPath, executable : str):
+        nonlocal content
+        content = re.sub(r"/\*TestGDBComms\*/", gdbComms, content)
         fPath.write(textwrap.dedent(content))
         cmd = [f"gdb -batch "
                f"{executable} "
--- tests/test_ViewProperties.py
+++ tests/test_ViewProperties.py
@@ -3,16 +3,13 @@
 # vim:fenc=utf-8
 #
 # Copyright 2020 Char Aznable <aznable.char.0083@gmail.com>
-# Description: Utilities for pretty-printing Kokkos::View
+# Description: Test basic view properties retrieve from the GDB debug session
 #
 # Distributed under terms of the 3-clause BSD license.
 import re
 import numpy as np
 
 shape = (3, 4, 5)
-strShape = ",".join(map(str, shape))
-layout = "Kokkos::LayoutRight"
-span = np.array(shape).prod()
 cpp = f"""
     #include <Kokkos_Core.hpp>
 
@@ -21,7 +18,8 @@ cpp = f"""
     int main(int argc, char* argv[]) {{
       Kokkos::initialize(argc,argv); {{
 
-      Kokkos::View<float***,{layout}> v("v",{strShape});
+      Kokkos::View<float***, /*TestViewLayoutTparam*/> v("v",
+        /*TestViewLayoutCtor*/);
 
       Kokkos::parallel_for(v.span(), KOKKOS_LAMBDA (const int i)
         {{ *(v.data()+i) = i; }});
@@ -31,27 +29,31 @@ cpp = f"""
       }} Kokkos::finalize();
 
     }}
+"""
+
+gdbinit = f"""
+    set confirm off
+
+    b breakpoint()
+      commands
+        silent
+        frame 1
+      end
+
+    run
+
+    /*TestGDBComms*/
+    quit
     """
 
 def testLayout(generateCMakeProject, runGDB):
-    p, _, fTest = generateCMakeProject
+    p, _, fTest, layout, _, _ = generateCMakeProject
     fGDB = p.join("testLayout.gdbinit")
     r, resultStr = runGDB(
         """
         py from GDBKokkos.printView import getKokkosViewLayout
-        set confirm off
-
-        b breakpoint()
-          commands
-            silent
-            frame 1
-          end
-
-        run
-
         py v = gdb.parse_and_eval('v')
         py print(getKokkosViewLayout(v))
-        quit
 
         """,
         fGDB, f"{fTest.realpath().strpath}"
@@ -60,24 +62,13 @@ def testLayout(generateCMakeProject, runGDB):
 
 
 def testExtent(generateCMakeProject, runGDB):
-    p, _, fTest = generateCMakeProject
+    p, _, fTest, _, shape, _ = generateCMakeProject
     fGDB = p.join("testExtent.gdbinit")
     r, resultStr = runGDB(
         """
         py from GDBKokkos.printView import getKokkosViewExtent
-        set confirm off
-
-        b breakpoint()
-          commands
-            silent
-            frame 1
-          end
-
-        run
-
         py v = gdb.parse_and_eval('v')
         py print(getKokkosViewExtent(v))
-        quit
 
         """,
         fGDB, f"{fTest.realpath().strpath}"
@@ -86,55 +77,48 @@ def testExtent(generateCMakeProject, runGDB):
     assert (result == np.array(shape)).all(), f"Wrong output extent: {r.stdout}"
 
 
+def testSpan(generateCMakeProject, runGDB):
+    p, _, fTest, layout, shape, strides = generateCMakeProject
+    fGDB = p.join("testSpan.gdbinit")
+    r, resultStr = runGDB(
+        """
+        py from GDBKokkos.printView import getKokkosViewSpan
+        py v = gdb.parse_and_eval('v')
+        py print(getKokkosViewSpan(v))
+
+        """,
+        fGDB, f"{fTest.realpath().strpath}"
+        )
+    iMax = np.array(strides).argmax()
+    expected = shape[iMax] * strides[iMax]
+    assert int(resultStr) == expected, "Wrong output Span: {r.stdout}"
+
+
 def testStrides(generateCMakeProject, runGDB):
-    p, _, fTest = generateCMakeProject
-    fGDB = p.join("testExtent.gdbinit")
+    p, _, fTest, layout, shape, strides = generateCMakeProject
+    fGDB = p.join("testStrides.gdbinit")
     r, resultStr = runGDB(
         """
         py from GDBKokkos.printView import getKokkosViewStrides
-        set confirm off
-
-        b breakpoint()
-          commands
-            silent
-            frame 1
-          end
-
-        run
-
         py v = gdb.parse_and_eval('v')
         py print(getKokkosViewStrides(v))
-        quit
 
         """,
         fGDB, f"{fTest.realpath().strpath}"
         )
     result = np.array(re.sub(r"\D+", " ", resultStr).split(), dtype=int)
-    expected = np.ones_like(shape, dtype=int)
-    expected[1:] = shape[::-1][:-1]
-    expected = np.cumprod(expected)[::-1]
+    expected = np.array(strides)
     assert (result == expected).all(), f"Wrong output strides: {r.stdout}"
 
 
 def testTraits(generateCMakeProject, runGDB):
-    p, _, fTest = generateCMakeProject
+    p, _, fTest, layout, _, _ = generateCMakeProject
     fGDB = p.join("testTraits.gdbinit")
     r, resultStr = runGDB(
         """
         py from GDBKokkos.printView import getKokkosViewTraits
-        set confirm off
-
-        b breakpoint()
-          commands
-            silent
-            frame 1
-          end
-
-        run
-
         py v = gdb.parse_and_eval('v')
         py print(getKokkosViewTraits(v))
-        quit
 
         """,
         fGDB, f"{fTest.realpath().strpath}"
@@ -144,24 +128,13 @@ def testTraits(generateCMakeProject, runGDB):
 
 
 def testValueType(generateCMakeProject, runGDB):
-    p, _, fTest = generateCMakeProject
+    p, _, fTest, _, _, _ = generateCMakeProject
     fGDB = p.join("testValueType.gdbinit")
     r, resultStr = runGDB(
         """
         py from GDBKokkos.printView import getKokkosViewValueType
-        set confirm off
-
-        b breakpoint()
-          commands
-            silent
-            frame 1
-          end
-
-        run
-
         py v = gdb.parse_and_eval('v')
         py print(getKokkosViewValueType(v))
-        quit
 
         """,
         fGDB, f"{fTest.realpath().strpath}"
@@ -170,58 +143,31 @@ def testValueType(generateCMakeProject, runGDB):
         "Wrong output ValueType: {r.stdout}"
 
 
-def testSpan(generateCMakeProject, runGDB):
-    p, _, fTest = generateCMakeProject
-    fGDB = p.join("testSpan.gdbinit")
-    r, resultStr = runGDB(
-        """
-        py from GDBKokkos.printView import getKokkosViewSpan
-        set confirm off
-
-        b breakpoint()
-          commands
-            silent
-            frame 1
-          end
-
-        run
-
-        py v = gdb.parse_and_eval('v')
-        py print(getKokkosViewSpan(v))
-        quit
-
-        """,
-        fGDB, f"{fTest.realpath().strpath}"
-        )
-    assert int(resultStr) == np.array(shape).prod(),\
-        "Wrong output Span: {r.stdout}"
-
-
 def testPrintView(generateCMakeProject, runGDB):
-    p, _, fTest = generateCMakeProject
+    p, _, fTest, layout, shape, strides = generateCMakeProject
     fGDB = p.join("testPrintView.gdbinit")
     r, resultStr = runGDB(
         """
         py import GDBKokkos
-        set confirm off
-
-        b breakpoint()
-          commands
-            silent
-            frame 1
-          end
-
-        run
-
         printView v --noIndex
-        quit
 
         """,
         fGDB, f"{fTest.realpath().strpath}"
         )
 
     result = np.array(resultStr.split()).astype(float).reshape(shape)
-    expected = np.arange(span).astype(float).reshape(shape)
+    iMax = np.array(strides).argmax()
+    span = shape[iMax] * strides[iMax]
+    expected = np.arange(span).astype(float)
+    if layout == "Kokkos::LayoutRight":
+        expected = expected.reshape(shape, order='C')
+    elif layout == "Kokkos::LayoutLeft":
+        expected = expected.reshape(shape, order='F')
+    elif layout == "Kokkos::LayoutStride":
+        expected = np.lib.stride_tricks.as_strided(
+            expected, shape=shape,
+            strides=np.array(strides) * expected.dtype.itemsize,
+            writeable=False)
     assert (result == expected).all(),\
         f"printView gives wrong view of shape {tuple}: {r.stdout}. Expected:\n"\
         f"{expected}"
